720ed7ef06d983e1c0cc0af53215cefd637df6a3
Felipe Lima <flf.felipe@gmail.com>
2020-07-26 22:48:44 -0300

    Commit inicial

---
+++ src/main/kotlin/com/tutorial/dev/quartzdemo/QuartzDemoApplication.kt
@@ -0,0 +1,11 @@
+package com.tutorial.dev.quartzdemo
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class QuartzDemoApplication
+
+fun main(args: Array<String>) {
+	runApplication<QuartzDemoApplication>(*args)
+}
---
+++ src/test/kotlin/com/tutorial/dev/quartzdemo/QuartzDemoApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.tutorial.dev.quartzdemo
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class QuartzDemoApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
